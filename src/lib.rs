225dcce51b56573a46f9eb3f71b5bb131c6d34fd
Miki <aivi.pol@gmail.com>
2023-03-17 19:44:36 +0100

    Part 3

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -35,30 +35,61 @@ impl ThreadPool {
             F: FnOnce() + Send + 'static
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool{
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers{
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers{
+            println!("Shutting down worker {} ", worker.id);
+            if let Some(thread) = worker.thread.take(){
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    pub fn new(id: usize, receiver : Arc<Mutex<mpsc::Receiver<Job>>> ) -> Worker {
+    pub fn new(id: usize, receiver : Arc<Mutex<mpsc::Receiver<Message>>> ) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver
+            let message = receiver
                 .lock()
                 .unwrap()
                 .recv()
                 .unwrap();
 
-            println!("Worker {} is executing a job.",id);
 
-            job();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} is executing a job.",id
+                    );
+                    job();}
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.",id);
+                    break;
+                }
+            }
+
         });
-        Worker { id, thread }
+        Worker { id, thread: Some(thread) }
     }
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message{
+    NewJob(Job),
+    Terminate,
+}
